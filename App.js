4646db5407c6fb0c70fbc4aca501ed085cfb827b
Beini W <wang.beini@northeastern.edu>
2024-04-14 23:49:47 -0400

    node

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import cors from 'cors';
 
 import UserRoutes from "./Users/routes.js";
+import Test from "./Test";
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 const DB_NAME = process.env.DB_NAME;
