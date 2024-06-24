b0adba7d3366fd992167984fb9465a8fbe45211b
yuezhang23 <jackiezy1002@gmail.com>
2024-06-24 17:08:44 -0400

    modify session

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
-if (process.env.NODE_ENV !== "development") {
+if (process.env.NODE_ENV === "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
     sameSite: "none",
